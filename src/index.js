b28c0c53666486a96402e3701d6dec31f8090b0a
AbderrahimAllani <abderrahim.allani@esprit.tn>
2024-07-11 15:41:16 +0100

    20 update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
   document.getElementById('root')
 );
